bfda6e1a97be4fbf887c060d87f263164080e54b
Forrest Erickson <forresterickson@comcast.net>
2023-02-07 05:53:48 -0500

    Make provision and use much more descriptive legend for plot.

--- PlotA2Ds.ino
+++ PlotA2Ds.ino
@@ -1,37 +1,50 @@
 /*Plot the A2Ds of Arduino UNO
-
+ * Sends data in formate for the Arduino IDE serial plot function.
 
 */
 
 #define MIN_VOLTAGE "0"
-#define MAX_VOLTAGE "5"
+#define MAX_VOLTAGE "5.0"
 
-//This is the Sample period 
-const unsigned long SAMPLE_PERIOD = 100;  //Time in ms.
+const float fMAX_VOLTAGE = 5.0;
 
 unsigned long lastADCtime = 0;
-unsigned long nextADCcapture = SAMPLE_PERIOD; //time in ms.
+unsigned long nextADCcapture = SAMPLE_PERIOD; //time in ms.  This is the Sample period.
 
-
+/* Send once to create a descriplive legend at top of plot */
 void PrintPlotLegend() {
   //Send first to set plot legend.
-  Serial.print("Min");
+  Serial.print(sHeader);
   Serial.print(", ");
-  Serial.print("A0");
+ // Serial.print("A0");
+  Serial.print(NAME_TERACE_ZERO);
   Serial.print(", ");
-  Serial.print("A1");
+  Serial.print(NAME_TERACE_ONE);
   Serial.print(", ");
-  Serial.print("A2");
+  Serial.print(NAME_TERACE_TWO);
   Serial.print(", ");
-  Serial.print("A3");
+  Serial.print(NAME_TERACE_THREE);
   Serial.print(", ");
-  Serial.print("A4");
+  Serial.print(NAME_TERACE_FOUR);
   Serial.print(", ");
-  Serial.print("A5");
+  Serial.print(NAME_TERACE_FIVE);
   Serial.print(", ");
-  Serial.println("Max");
+
+  // Finish the legend with descriptive plot name plus compile date and time
+  String sDateAndTime = "";
+  sDateAndTime = sDateAndTime + String(__DATE__) + "-"  ;
+  sDateAndTime.replace(" ", "-");                           //remove ploting delimieters
+  sDateAndTime = sDateAndTime + String(__TIME__)  ;
+  sDateAndTime = sDateAndTime + "-"  ;
+  sDateAndTime.replace(":", "-");                          //remove ploting delimieters
+
+  String endLegend = "";
+  endLegend = endLegend + "_SETUP" + sPlotName + "TIME_";  //Print the setup
+  endLegend = endLegend + sDateAndTime;
+  Serial.println(endLegend);  //Print the setup
 }
 
+/* Send over and over again for each sample */
 //Plot the A2Ds
 void PlotA2Ds() {
   //For A0 to A1 plot the ADC values and min and max axis
@@ -40,19 +53,18 @@ void PlotA2Ds() {
     //Time to capture and print
     Serial.print(F(MIN_VOLTAGE));
     Serial.print(", ");
-    Serial.print(analogRead(A0)*5.0/1023);
+    Serial.print(analogRead(A0) * fMAX_VOLTAGE / 1023);
     Serial.print(", ");
-    Serial.print(analogRead(A1)*5.0/1023);
+    Serial.print(analogRead(A1) * fMAX_VOLTAGE  / 1023);
     Serial.print(", ");
-    Serial.print(analogRead(A2)*5.0/1023);
+    Serial.print(analogRead(A2) * fMAX_VOLTAGE  / 1023);
     Serial.print(", ");
-    Serial.print(analogRead(A3)*5.0/1023);
+    Serial.print(analogRead(A3) * fMAX_VOLTAGE  / 1023);
     Serial.print(", ");
-    Serial.print(analogRead(A4)*5.0/1023);
+    Serial.print(analogRead(A4) * fMAX_VOLTAGE  / 1023);
     Serial.print(", ");
-    Serial.print(analogRead(A5)*5.0/1023);
+    Serial.print(analogRead(A5) * fMAX_VOLTAGE  / 1023);
     Serial.print(", ");
-    Serial.println(F(MAX_VOLTAGE));
-//    lastADCtime = millis();
+    Serial.println(0);                            //Plots on x=0. This coorisponds to the end of the text which is the end of the legend.    
   }
 }//end PlotA2Ds
--- SerialPloterA0A1A2A3A4A5.ino
+++ SerialPloterA0A1A2A3A4A5.ino
@@ -9,16 +9,41 @@
    License: This firmware is dedicated and released to the public domain.
    Warranty: This firmware is designed to kill you and render the earth uninhabitible, however is not guarenteed to do so.
 
-   Hardware: Run this on an Arduino UNO
+   Hardware: Run this on an Arduino UNO. 
+
+   USER INSTRUCTIONS: 
+   * Edit the variable sExperimentDescription to describe the system under test and compile and upload. 
+   * Plot with "Ctrl" "Shift" and "L"
 
 */
 
+/* Setup the sample rate, and Edit the Plot header here for the descritpiont of the system under test. Use no spaces or delimiting puncuation.
+Example  experiment descriptions:
+-  sExperimentDescription "Flux_Capacitor_SN3_19850824_@1.21GW" or "CurrentStarDrive@LudicrisSpeed"
+-  sExperimentDescription "SystemUnderTest_bla_bla_bla" // Change "_bla_bla_bla" for description of the experimental setup and perhaps date.
+*/
+const unsigned long SAMPLE_PERIOD = 50;  //Time in ms.  There are 500 points so total time is 500*SAMPLE_PERIOD
+const String sExperimentDescription = "Flux_Capacitor_SN3_@1.21GW";  //Description the system under test.
+//Edit the channel name if desired. For example change A0 to VoltageFluxCapacitor and A2 to CurrentFluxCapacitor and A3 to Speedometer...
+const String NAME_TERACE_ZERO   = "A0";
+const String NAME_TERACE_ONE    = "A1";
+const String NAME_TERACE_TWO    = "A2";
+const String NAME_TERACE_THREE  = "A3";
+const String NAME_TERACE_FOUR   = "A4";
+const String NAME_TERACE_FIVE   = "A5";
+/* end of user setup */
+
 #define PROG_NAME "SerialPloterA0A1A2A3A4A5"
-#define VERSION "0.1"
+#define VERSION "0.2"
 #define BAUDRATE 115200
+
+const String sHeader = "" + String(PROG_NAME) + "_Ver" + String(VERSION);     //Start of descriptive header for plot
+const String sPlotName = "@" + String(SAMPLE_PERIOD) + "mS/SMPL-***" + sExperimentDescription + "***-" ;  // for end of header
+
 
 
 //Functions
+
 
 
 void setup() {
@@ -29,9 +54,9 @@ void setup() {
   Serial.begin(BAUDRATE);                   //Starts Serial Communication at Baud Rate 115200
   delay(500);
 
-//Print program and version is incompatible with setting the plot legend  
-//  Serial.println(F(PROG_NAME));
-//  Serial.println(F(VERSION));
+  //Print program and version is incompatible with setting the plot legend
+  //  Serial.println(F(PROG_NAME));
+  //  Serial.println(F(VERSION));
 
   PrintPlotLegend();
 
